1ef10ad4c6cc576bb2dbc6f7b5fa9c96da451773
root <sdncoder@github.com>
2021-02-25 19:37:30 -0500

    idf build added configlets array

--- idf-template-build.lua
+++ idf-template-build.lua
@@ -4,21 +4,27 @@
 --
 local counter = 1 -- set table to first index for load of configlets
 build = {} -- create table  -- setup table "build"
+configlets = {'base-system.txt', 'base-security.txt'} -- 2 configlets
 --
--- load base security configlet
-io.input("base-security.txt") -- read external file
+
+
+-- load config sections from array configlets{}
+-- y max is the number of .txt called
+for y = 1, 2 do
+io.input(configlets[y]) -- read external file
 
 -- functions
 function readFile(build)
-
 repeat
- temp = io.read("*line")  -- read next line when you call io.read()
- if temp ~= "" then -- not EOF
-   build[counter] = temp -- write line into table
-   counter = counter + 1
- end
+  temp = io.read("*line")  -- read next line when you call io.read()
+   if temp ~= "" then -- not EOF
+    build[counter] = temp -- write line into table
+    counter = counter + 1
+    end
 until temp == nul
-end
+
+counter = 1  -- reset to top of next table
+end  -- end for loop
 
 -- call functions
 readFile(build)  -- pass file into build table
@@ -27,3 +33,4 @@ repeat
  print(build[x])
  x=x+1
  until build[x]==nul -- EOF
+end
